53d9cda2a64a1d0eceb22bcc48dd31521b0eec8b
White-Green <43771790+White-Green@users.noreply.github.com>
2025-01-15 07:49:42 +0900

    add cbmc to path

--- scripts/setup/windows/install_cbmc.sh
+++ scripts/setup/windows/install_cbmc.sh
@@ -21,5 +21,8 @@ curl -L -o "$FILE" "$URL"
 MSYS_NO_PATHCONV=1 msiexec /i "$FILE" /qn /norestart /l* installer_log.txt
 cat installer_log.txt
 export PATH="/c/Program Files/cbmc/bin:$PATH"
+echo <<EOF >> ~/.bashrc
+export PATH="/c/Program Files/cbmc/bin:$PATH"
+EOF
 cbmc --version
 rm $FILE
