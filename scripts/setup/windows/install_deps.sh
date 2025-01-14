03789bf4a0e410d475c70974527dca22587f9096
White-Green <43771790+White-Green@users.noreply.github.com>
2025-01-15 07:42:53 +0900

    skip kissat installation in windows

--- scripts/setup/windows/install_deps.sh
+++ scripts/setup/windows/install_deps.sh
@@ -8,5 +8,3 @@ set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ${SCRIPT_DIR}/install_cbmc.sh
-# The Kissat installation script is platform-independent, so is placed one level up
-${SCRIPT_DIR}/../install_kissat.sh
