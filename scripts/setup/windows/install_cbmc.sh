dfc88fa2b70520e3f86029df65f19583f7fe3d7d
White-Green <43771790+White-Green@users.noreply.github.com>
2025-01-15 06:27:42 +0900

    test

--- scripts/setup/windows/install_cbmc.sh
+++ scripts/setup/windows/install_cbmc.sh
@@ -19,7 +19,7 @@ set -x
 
 curl -L -o "$FILE" "$URL"
 touch install_log
-sudo msiexec /i "$FILE" /passive /quiet /norestart /l! install_log &
+msiexec /i "$FILE" /passive /quiet /qn /norestart /l*! install_log &
 while [ "$(jobs -r | wc -l)" -gt 0 ]; do
   tail -n 10 install_log
   sleep 1
