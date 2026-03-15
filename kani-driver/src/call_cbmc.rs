51967872da64f8c6e5bef11c666d7bbcbdff1e14
White-Green <43771790+White-Green@users.noreply.github.com>
2026-03-15 16:34:37 +0900

    ci(windows): build cbmc from source with minisat only

--- kani-driver/src/call_cbmc.rs
+++ kani-driver/src/call_cbmc.rs
@@ -28,7 +28,7 @@ use crate::util::render_command;
 
 /// Windows prebuilt CBMC packages in CI do not ship CaDiCaL.
 #[cfg(windows)]
-static DEFAULT_SOLVER: CbmcSolver = CbmcSolver::Cadical;
+static DEFAULT_SOLVER: CbmcSolver = CbmcSolver::Minisat;
 
 /// We use CaDiCaL by default on non-Windows platforms.
 #[cfg(not(windows))]
