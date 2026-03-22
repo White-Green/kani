cf4bfdbce70abe4a69213b550b1a11218808bd37
White-Green <43771790+White-Green@users.noreply.github.com>
2026-03-23 02:39:14 +0900

    tests(windows): re-enable multimodule harness name with filtered run

--- tests/kani/FunctionCall_MultiModuleSameHarnessName/main.rs
+++ tests/kani/FunctionCall_MultiModuleSameHarnessName/main.rs
@@ -1,7 +1,5 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// ignore-windows
-
 //! This test checks if Kani can handle multiple harnesses in
 //! different directories (first, second, main) that share the same
 //! harness name (check). Previously, this caused Kani to crash. See
